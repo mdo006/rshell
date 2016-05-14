54b1b7a99c866230b9976e622a26414750737641
mdo006 <mdo006@ucr.edu>
2016-05-14 03:39:40 +0000

    2nd try!

--- src/assn2.cpp
+++ src/assn2.cpp
@@ -34,6 +34,11 @@ void execute (string &pass, int &valid)
 				temp = temp + pass.at(j);
 			}
 		}
+		if(temp == "exit")
+		{
+			cout << "exiting!" << endl;
+			exit(0);
+		}
 		command.push_back(temp);
 		temp.clear();
 		//so we can start basing on where we left off in pass string
@@ -141,115 +146,119 @@ void parse (vector<string> &input, int &valid)
 
 int main()
 {
-	//command line inputted by user
-	char userInput[100];
-	
-	//print a command prompt (e.g. $)
-	cout << "$ ";
-	//read in command as one line
-	cin.getline(userInput, 100);
-	
-	char userInput_no_comments[100];
-	
-	//ignore everything after '#' (everything after '#' is a comment)
-	for (int i = 0; userInput[i] != '\0'; ++i)
+	//loop until the user exits
+	while (1)
 	{
-		if (userInput[i] == '#')
+		//command line inputted by user
+		char userInput[100];
+	
+		//print a command prompt (e.g. $)
+		cout << "$ ";
+		//read in command as one line
+		cin.getline(userInput, 100);
+		
+		char userInput_no_comments[100];
+		
+		//ignore everything after '#' (everything after '#' is a comment)
+		for (int i = 0; userInput[i] != '\0'; ++i)
 		{
-			break;
+			if (userInput[i] == '#')
+			{
+				break;
+			}
+			
+			userInput_no_comments[i] = userInput[i];
+			userInput_no_comments[i + 1] = '\0';
 		}
 		
-		userInput_no_comments[i] = userInput[i];
-		userInput_no_comments[i + 1] = '\0';
-	}
-	
-	//get connectors from userInput and store it into a vector
-	//but in this vector && and || are stored as & & and | |
-	vector<char> vector_connectors_separated;
-	
-	//FIX THIS LATER IF THERE IS TIME 
-	//DOES NOT ACCOUNT FOR JUST '&', JUST '|', OR MORE THAN TWO '&'s OR '|'s
-	//ALSO DOES NOT ACCOUNT FOR MORE THAN ONE ';' IN A ROW
-	for (int i = 0; userInput_no_comments[i] != '\0'; ++i)
-	{
-		if (userInput_no_comments[i] == ';'|| userInput_no_comments[i] == '&' 
-			|| userInput_no_comments[i] == '|')
+		//get connectors from userInput and store it into a vector
+		//but in this vector && and || are stored as & & and | |
+		vector<char> vector_connectors_separated;
+		
+		//FIX THIS LATER IF THERE IS TIME 
+		//DOES NOT ACCOUNT FOR JUST '&', JUST '|', OR MORE THAN TWO '&'s OR '|'s
+		//ALSO DOES NOT ACCOUNT FOR MORE THAN ONE ';' IN A ROW
+		for (int i = 0; userInput_no_comments[i] != '\0'; ++i)
 		{
-			vector_connectors_separated.push_back(userInput[i]);
-		}
-	}
-	
-	//NOW COMBIN & and & into && and | and | into ||
-	vector<string> vector_connectors;
-	
-	for (int i = 0; i < vector_connectors_separated.size(); ++i)
-	{
-		if (vector_connectors_separated.at(i) == '&')
-		{
-			vector_connectors.push_back("&&");
-			++i;
-		}
-		else if (vector_connectors_separated.at(i) == '|')
-		{
-			vector_connectors.push_back("||");
-			++i;
-		}
-		else if (vector_connectors_separated.at(i) == ';')
-		{
-			vector_connectors.push_back(";");
-		}
-	}
-	
-	//use strtok to separate input into "tokens"
-	char* tokens; //command and its arguments
-	char delimiters[] = ";&&||";
-	tokens = strtok(userInput_no_comments, delimiters);
-	
-	//stores all the commands and its arguments as separate "tokens"
-	vector<char*> vector_tokens;
-	
-	while (tokens != NULL)
-	{
-		// int position =0;
-		// while(())
-		//remove first whitespace in token
-		if (tokens[0] == ' ')
-		{
-			++tokens; //go to next character (ignore 1st white space)
+			if (userInput_no_comments[i] == ';'|| userInput_no_comments[i] == '&' 
+				|| userInput_no_comments[i] == '|')
+			{
+				vector_connectors_separated.push_back(userInput[i]);
+			}
 		}
 		
-		vector_tokens.push_back(tokens);
-		tokens = strtok(NULL, delimiters);
+		//NOW COMBIN & and & into && and | and | into ||
+		vector<string> vector_connectors;
+		
+		for (int i = 0; i < vector_connectors_separated.size(); ++i)
+		{
+			if (vector_connectors_separated.at(i) == '&')
+			{
+				vector_connectors.push_back("&&");
+				++i;
+			}
+			else if (vector_connectors_separated.at(i) == '|')
+			{
+				vector_connectors.push_back("||");
+				++i;
+			}
+			else if (vector_connectors_separated.at(i) == ';')
+			{
+				vector_connectors.push_back(";");
+			}
+		}
+		
+		//use strtok to separate input into "tokens"
+		char* tokens; //command and its arguments
+		char delimiters[] = ";&&||";
+		tokens = strtok(userInput_no_comments, delimiters);
+		
+		//stores all the commands and its arguments as separate "tokens"
+		vector<char*> vector_tokens;
+		
+		while (tokens != NULL)
+		{
+			// int position =0;
+			// while(())
+			//remove first whitespace in token
+			if (tokens[0] == ' ')
+			{
+				++tokens; //go to next character (ignore 1st white space)
+			}
+			
+			vector_tokens.push_back(tokens);
+			tokens = strtok(NULL, delimiters);
+		}
+		
+		//convert the tokens from char* to string
+		vector<string> vector_tokens_str;
+	
+		for (int i = 0; i < vector_tokens.size(); ++i)
+		{
+			char* s = vector_tokens.at(i);
+			string str(s);
+			vector_tokens_str.push_back(str);
+		}
+		
+		//put everything into one vector called input
+		vector<string> input;
+		
+		for (int i = 0; i < vector_tokens_str.size() - 1; ++i)
+		{
+			input.push_back(vector_tokens_str.at(i));
+			input.push_back(vector_connectors.at(i));
+		}
+		
+		//add in the last command 
+		/*if we put this in the for loop above, it would go out of range, because
+		  vector_connectors is smalled than vector_tokens_str by one*/
+		int size = vector_tokens_str.size();
+		input.push_back(vector_tokens_str.at(size - 1));
+		
+		//run execute on commands
+		int valid = 1;
+		parse(input, valid);
 	}
-	
-	//convert the tokens from char* to string
-	vector<string> vector_tokens_str;
-
-	for (int i = 0; i < vector_tokens.size(); ++i)
-	{
-		char* s = vector_tokens.at(i);
-		string str(s);
-		vector_tokens_str.push_back(str);
-	}
-	
-	//put everything into one vector called input
-	vector<string> input;
-	
-	for (int i = 0; i < vector_tokens_str.size() - 1; ++i)
-	{
-		input.push_back(vector_tokens_str.at(i));
-		input.push_back(vector_connectors.at(i));
-	}
-	
-	//add in the last command 
-	/*if we put this in the for loop above, it would go out of range, because
-	  vector_connectors is smalled than vector_tokens_str by one*/
-	int size = vector_tokens_str.size();
-	input.push_back(vector_tokens_str.at(size - 1));
-	
-	//run execute on commands
-	int valid = 1;
-	parse(input, valid);
 	
 	return 0;
 }
